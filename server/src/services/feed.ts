235301c06458453c6ac00cf9c5947ae24869b3f9
faminimark <markfamini@gmail.com>
2025-05-14 10:26:48 -0700

    Add TODO for prisma raw query

--- server/src/services/feed.ts
+++ server/src/services/feed.ts
@@ -6,5 +6,10 @@ export const getFeed = async (
     query?: Prisma.posts$categoriesArgs | Prisma.posts$post_tagsArgs
   ): Promise<Posts[]> => {
     const posts: Posts[] = await prisma.posts.findMany();
+
+    // TODO: Add entry to post_tags and category
+    // const posts: Posts[] = await prisma.$queryRaw<Posts[]>`
+    // select * from posts
+    // `
     return serializer(posts);
   };
