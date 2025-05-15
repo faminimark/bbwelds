4f9a847263e242353e5301464993435aa6d78c69
faminimark <markfamini@gmail.com>
2025-05-15 13:30:06 -0700

    Clean up

--- client/src/lib/components/Feed/Footer.svelte
+++ client/src/lib/components/Feed/Footer.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { ArrowUpIcon, ArrowDownIcon, ChatBubbleLeftIcon, ShareIcon } from 'heroicons-svelte/24/outline'
+    import { ArrowUpIcon, ArrowDownIcon, ChatBubbleLeftIcon } from 'heroicons-svelte/24/outline'
 </script>
 
 <div class="flex flex-row gap-4">
@@ -7,5 +7,4 @@
     <button type="button" class="flex flex-row gap-3 p-2 hover:bg-gray-100 rounded-md text-gray-400"><ArrowUpIcon class=" w-5 h-5"/> 25</button>
     <button type="button" class="flex flex-row gap-3 p-2 hover:bg-gray-100 rounded-md text-gray-400"><ArrowDownIcon class=" w-5 h-5"/></button>
     <button type="button" class="flex flex-row gap-3 p-2 hover:bg-gray-100 rounded-md text-gray-400"><ChatBubbleLeftIcon class="w-5 h-5 "/> 55 comments</button>
-
 </div>
