a661ee18bfc687e7d9720ecd09f5128a81137801
faminimark <markfamini@gmail.com>
2025-05-16 09:24:36 -0700

    Change responsive size

--- client/src/routes/login/+page.svelte
+++ client/src/routes/login/+page.svelte
@@ -5,7 +5,7 @@ const googleLogo = '/google.svg';
 </script>
 
 <div class="grid grid-cols-2 max-md:flex max-md:flex-col-reverse max-md:h-screen max-md:justify-end">
-    <div class="bg-emerald-800 text-white p-15 flex flex-col gap-10 max-sm:hidden">
+    <div class="bg-emerald-800 text-white p-15 flex flex-col gap-10 max-md:hidden">
         <h2 class="text-5xl font-semibold">Hello, Welcome back</h2>
         <div class="flex flex-col text-center gap-4">
             <p>Do not have an account?</p> 
@@ -13,7 +13,7 @@ const googleLogo = '/google.svg';
         </div>
     </div>
     <div class="flex flex-col gap-7 p-6">
-        <a href="/register" class="font-bold border-1 border-black rounded-2xl p-3 w-[120px] text-center self-end hidden max-sm:block">Sign up</a>
+        <a href="/register" class="font-bold border-1 border-black rounded-2xl p-3 w-[120px] text-center self-end hidden max-md:block">Sign up</a>
         <form class="flex flex-col gap-3">
             <span class="text-sm text-gray-700">Email</span>
             <Text name="email" placeholder="Email Address" />
