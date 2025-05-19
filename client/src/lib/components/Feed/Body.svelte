54b0cdcd7efcc9785c814ef2fb40d038dcf39040
faminimark <markfamini@gmail.com>
2025-05-19 14:29:45 -0700

    Fix body

--- client/src/lib/components/Feed/Body.svelte
+++ client/src/lib/components/Feed/Body.svelte
@@ -2,7 +2,7 @@
     let { description, post_id } = $props()
 </script>
 
-<div class="flex flex-col min-w-380px">
+<div class="flex flex-col min-w-280px">
     <a href={`/post/${post_id}`} class="flex justify-center bg-gray-100">
         <img aria-label="feed" alt="feed" src="https://picsum.photos/400" class="max-h-380px "/>
     </a>
