8f7db9afb116e32191516b352c46754e40b73438
faminimark <markfamini@gmail.com>
2025-05-22 14:15:02 -0700

    I need token to save

--- server/src/services/auth.ts
+++ server/src/services/auth.ts
@@ -1,15 +1,17 @@
 import { verifyPassword } from '../utils/password';
 import { PrismaClient, users as User } from '@prisma/client'
+import { sign } from 'hono/jwt'
+
 const prisma = new PrismaClient();
 
-export const login = async (username: string, password: string): Promise<{verified: boolean, error: string | undefined }  > =>  {
+export const login = async (username: string, password: string): Promise<{verified: boolean, error: string | undefined, token: string | undefined }  > =>  {
     const user: User | null = await prisma.users.findUnique({
             where: {
                 email: username
             }
         });
 
-    if(!user) return { verified: false, error: 'User not found' }
+    if(!user) return { verified: false, error: 'User not found', token: undefined }
 
     const auth = await prisma.auth.findFirst({
             where: {
@@ -17,11 +19,14 @@ export const login = async (username: string, password: string): Promise<{verifi
             }
         })
     
-    if(!auth) return { verified: false, error: 'User not registered' }
+    if(!auth) return { verified: false, error: 'User not registered', token: undefined }
 
     const verified = await verifyPassword(password, auth?.password)
     
-    if(!verified) return { verified: false, error: 'Incorrect Password' }
+    if(!verified) return { verified: false, error: 'Incorrect Password', token: undefined }
     
-    return { verified: true, error: undefined }
+    const { f_name: fname, user_id } = user;
+    const token = await sign({ fname, user_id },'REDACTED this will be on the env later too lazy to set it up');
+
+    return { verified: true, error: undefined, token }
 }
