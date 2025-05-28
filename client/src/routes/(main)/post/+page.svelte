481d05d27742dd0d21ed27c7e81bfbe1e9a21975
faminimark <markfamini@gmail.com>
2025-05-27 17:19:32 -0700

    forgot another context to prop

--- client/src/routes/(main)/post/+page.svelte
+++ client/src/routes/(main)/post/+page.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-    import { getContext } from 'svelte';
     import BackButton from '$lib/components/BackButton.svelte'
     import Card from '$lib/components/Card.svelte'
     import { Text, TextArea, Select } from '$lib/components/Fields'
-    const options: { displayValue: string; value: string; }[] = getContext('category')
+    let { data } = $props();
 </script>
   
 <div class="flex flex-col gap-3 max-w-2xl self-center max-sm:w-full  min-sm:min-w-2xl">
@@ -12,7 +11,7 @@
     <Card>
         <form class="flex flex-col gap-3">
             <Text name={'title'} placeholder={'Title'} max={50}></Text>
-            <Select name={'category'} placeholder="Pick a trade" options={options} select={(val) => { console.log({val})}}/>
+            <Select name={'category'} placeholder="Pick a trade" options={data.categories} select={(val) => { console.log({val}) }}/>
             <label for="file">
                 <input name="file" type="file" aria-label="Choose an image" placeholder="Select image" class="min-w-[280px] border-1 border-gray-300 rounded-md p-4 w-full"/>
             </label>
