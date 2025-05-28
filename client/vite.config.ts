c73db92194543bc9ec277fd36bb0c0d773d59d0b
faminimark <markfamini@gmail.com>
2025-05-28 09:52:47 -0700

    prettier

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-	server: {port: 3000},
+	server: { port: 3000 },
 	plugins: [
 		sveltekit(),
 		tailwindcss()
