f2a43c0231ebf1a641507515f01873f833bb4b9c
faminimark <markfamini@gmail.com>
2025-05-28 10:01:22 -0700

    refactor after copy paste from docs

--- client/src/params/category.ts
+++ client/src/params/category.ts
@@ -1,5 +1,10 @@
 import type { ParamMatcher } from '@sveltejs/kit';
 
-export const match = ((param: string): param is ('category' | 'tag') => {
-	return param === 'category' || param === 'tag';
+const categories = ['category', 'tag', 'new', 'featured'] as const;
+const categorySet = new Set(categories);
+
+type Category = typeof categories[number];
+
+export const match = ((param: string): param is Category => {
+	return categorySet.has(param as Category);
 }) satisfies ParamMatcher;
