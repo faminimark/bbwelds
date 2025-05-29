22910ec8bc4405f6a500956b4f62f8cf6d9f3cb6
faminimark <markfamini@gmail.com>
2025-05-28 21:51:50 -0700

    add shadow to the nav

--- client/src/lib/components/Nav.svelte
+++ client/src/lib/components/Nav.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
     const logo = '/bb-logo.svg';
     import { PencilIcon } from 'lucide-svelte';
-    const { isLoggedIn } = $props()
+    const { isLoggedIn, user_id } = $props()
 </script>
 
-<div class="pt-[7px] sticky top-0 right-0 left-0 bg-white">
+<div class="pt-[7px] sticky top-0 right-0 left-0 bg-white shadow-md">
     <div class="flex flex-row items-center justify-between pb-[5px] max-sm:px-2 max-sm:pb-[6px] px-8">
         <div class="flex flex-row">
             <a aria-label="bbwelds logo" href="/" class="flex flex-row items-center">
@@ -20,7 +20,7 @@
             {#if !isLoggedIn}
                 <a href="/login">Login</a>
             {:else}
-                <a href="/profile">Profile</a>
+                <a href="/profile/{user_id}">Profile</a>
             {/if}
             <a href="/post" class="px-[9px] py-[5px] cursor-pointer rounded-sm bg-blue-500 text-white font-semibold flex gap-2 items-center">
                 <PencilIcon class="h-4 w-4"/> Post
