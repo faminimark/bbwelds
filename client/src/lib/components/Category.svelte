1cd154bc33abb394d644c30dc2ea48d3b2f01ccb
faminimark <markfamini@gmail.com>
2025-06-02 13:24:45 -0700

    Changed category into an enum

--- client/src/lib/components/Category.svelte
+++ client/src/lib/components/Category.svelte
@@ -5,7 +5,7 @@
 <div class="max-sm:p-1 flex gap-2 flex-col">
 	<h3 class="text-xl text-gray-700 font-semibold max-sm:text-md max-sm:font-normal">Filter by category</h3>
 	<div class="flex max-lg:overflow-auto">
-		<ul class="flex flex-col max-lg:flex-row gap-2 min-h-[45px] pt-2 w-full">
+		<ul class="flex flex-col max-lg:flex-row gap-2 min-h-[45px] pt-2 w-full capitalize">
 			{#each categories as category}
 			<li>
 				<a href='/category/{category.value}' class="font-semibold text-gray-500 max-sm:text-sm max-lg:border-gray-400 max-lg:border-1 max-lg:shadow-md p-1 rounded-xs text-nowrap">
--- client/src/routes/(main)/+layout.server.ts
+++ client/src/routes/(main)/+layout.server.ts
@@ -2,16 +2,16 @@ import { SERVER_URL } from '$env/static/private';
 
 export const load = async ({ params, cookies }) => {
     const user_id = cookies.get('user_id') ?? params.id;
-    const user = user_id ? await (await fetch(`${SERVER_URL}/user/${user_id}`)).json(): null
+
+    const [user, categories] = await Promise.all([
+        user_id ? await (await fetch(`${SERVER_URL}/user/${user_id}`)).json(): null, 
+        await (await fetch(`${SERVER_URL}/category`)).json(), 
+    ])
+
     const isLoggedIn = user_id ? true : false
+    
 	return { 
-        categories: [
-            {value: 'weld', displayValue: 'Welding'},
-            {value: 'elect', displayValue: 'Electrician'},
-            {value: 'automotive', displayValue: 'Automotive'},
-            {value: 'construction', displayValue: 'Construction'},
-            {value: 'ironworker', displayValue: 'Iron Worker'}
-        ], 
+        categories: categories.data, 
         user: user?.data,
         isLoggedIn
     }
