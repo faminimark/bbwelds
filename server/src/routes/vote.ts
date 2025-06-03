a3dcf7b1b0237371b256ef90aba17d8cc9ffbcab
faminimark <markfamini@gmail.com>
2025-06-03 13:20:45 -0700

    votes

--- server/src/routes/vote.ts
+++ server/src/routes/vote.ts
@@ -1,12 +1,17 @@
 import { Hono } from 'hono'
+import { upvote, downvote } from '../services/vote'
 
 const vote = new Hono()
 
-vote.post('/upvote', (c) => {
+vote.post('/upvote', async (c) => {
+    const data = await c.req.json()
+    await upvote(data)
     return c.json({ success: true });
 })
 
-vote.post('/downvote', (c) => {
+vote.post('/downvote', async (c) => {
+    const data = await c.req.json()
+    await downvote(data)
     return c.json({ success: true });
 })
 
--- server/src/services/vote.ts
+++ server/src/services/vote.ts
@@ -1,9 +1,74 @@
-import { Prisma } from "@prisma/client";
+import { PrismaClient, image_type } from '@prisma/client'
+import { HTTPException } from 'hono/http-exception';
 
+const prisma = new PrismaClient();
 
-// export const getUsers = async (
-//     query: Prisma.UserWhereInput
-//   ): Promise<SearchUserResponse[]> => {
-//     const users = await prisma.user.findMany({ where: query });
-//     return users.map((b) => searchUserResponse.parse(b));
-//   };
+export const upvote = async (
+    { id, type }: {id: string | number, type: image_type}
+): Promise<{success: boolean}> => {
+    const vote = await prisma.votes.findFirst({
+        where: {
+            voteable_id: Number(id),
+            voteable_type : type as image_type
+        },
+    });
+
+    if(!vote) {
+        await prisma.votes.create({
+            data: {
+                voteable_id: Number(id),
+                voteable_type : type as image_type,
+                upvote: 1,
+                downvote: 0
+            }
+        })
+    } else {
+        await prisma.votes.update({
+            where: {
+                vote_id: vote.vote_id,
+            },
+            data: {
+                upvote: {
+                    increment: 1
+                }
+            }
+        })
+    }
+
+    return { success: true };
+};
+
+export const downvote = async (
+    { id, type }: {id: string | number, type: image_type}
+): Promise<{success: boolean}> => {
+    const vote = await prisma.votes.findFirst({
+        where: {
+            voteable_id: Number(id),
+            voteable_type : type as image_type
+        },
+    });
+
+    if(!vote) {
+        await prisma.votes.create({
+            data: {
+                voteable_id: Number(id),
+                voteable_type : type as image_type,
+                downvote: 1,
+                upvote: 0
+            }
+        })
+    } else {
+        await prisma.votes.update({
+            where: {
+                vote_id: vote.vote_id,
+            },
+            data: {
+                downvote: {
+                    increment: 1
+                }
+            }
+        })
+    }
+
+    return { success: true };
+};
