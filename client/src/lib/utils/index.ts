4ec294db672107adbe409e51563e74433801d648
faminimark <markfamini@gmail.com>
2025-06-03 20:52:21 -0700

    same with this

--- client/src/lib/utils/index.ts
+++ client/src/lib/utils/index.ts
@@ -1,31 +1,8 @@
-import { browser } from "$app/environment";
-
 export const backHandler = () => {
     window.history.back();
 }
 
 export const validateEmail = (input: string) => {
-const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-
-    return regex.test(input) ? null : 'Please enter a correct email format'
-}
-
-export const setCookie = (name: string, value: string, days: number) => {
-    const expires = new Date();
-    expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
-    document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
-}
-
-export const getCookie = (name: string) => {
-  if(browser) {
-    const cookies = document.cookie.split(';');
-    for (let cookie of cookies) {
-      const [cookieName, cookieValue] = cookie.trim().split('=');
-      if (cookieName === name) {
-        return decodeURIComponent(cookieValue);
-      }
-    }
-  }
-
-  return null;
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+  return regex.test(input) ? null : 'Please enter a correct email format'
 }
