f3b67a5478796e1c4e568242ad66c9ccd4cb130a
faminimark <markfamini@gmail.com>
2025-06-06 16:16:37 -0700

    why is it even there to begin with?

--- client/src/routes/(main)/+layout.server.ts
+++ client/src/routes/(main)/+layout.server.ts
@@ -1,13 +1,13 @@
 import { SERVER_URL } from '$env/static/private';
 
-export const load = async ({ params, cookies }) => {
-    const user_id = cookies.get('user_id') ?? params.id;
+export const load = async ({ cookies }) => {
+    const user_id = cookies.get('user_id') ?? undefined;
 
     const [user, categories] = await Promise.all([
         user_id ? await (await fetch(`${SERVER_URL}/user/${user_id}`)).json(): null, 
         await (await fetch(`${SERVER_URL}/category`)).json(), 
     ])
-
+    
     const isLoggedIn = user_id ? true : false
     
 	return { 
