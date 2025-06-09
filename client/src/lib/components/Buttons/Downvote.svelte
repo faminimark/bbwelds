580f10c98916a28fbf4c8ac54bf65a9866d2f018
faminimark <markfamini@gmail.com>
2025-06-09 09:49:57 -0700

    votes

--- client/src/lib/components/Buttons/Downvote.svelte
+++ client/src/lib/components/Buttons/Downvote.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { ArrowDownIcon } from 'lucide-svelte'
 
-    let { liked = false } = $props()
+    let { disliked = false } = $props()
 </script>
 
-<button type="submit" class="flex flex-row  items-center gap-3 p-2 { liked ? 'hover:bg-blue-100 text-black-400' : 'hover:bg-gray-100 text-gray-400' } rounded-md cursor-pointer" value="downvote" name='voteAction'><ArrowDownIcon class=" w-5 h-5"/></button>
+<button type="submit" class="flex flex-row  items-center gap-3 p-2 { disliked ? 'hover:bg-red-100 text-red-400' : 'hover:bg-gray-100 text-gray-400' } rounded-md cursor-pointer" value="downvote" name='voteAction'><ArrowDownIcon class=" w-5 h-5"/></button>
--- client/src/lib/components/Feed/Footer.svelte
+++ client/src/lib/components/Feed/Footer.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
     import { MessageSquare } from 'lucide-svelte'
     import { DownvoteButton, UpvoteButton } from '$lib/components/Buttons'
-  import { enhance } from '$app/forms';
+    import { enhance } from '$app/forms';
     let { post_id, votes = undefined } = $props();
-
+    const  liked = votes?.user_votes?.[0]?.vote_type === 'upvote'
+    const disliked = votes?.user_votes?.[0]?.vote_type === 'downvote'
 </script>
 
 <div class="flex flex-row gap-4">
     <form class="flex" method="POST" action="?/vote" use:enhance>
-        <UpvoteButton count={votes?.upvote}/>
-        <DownvoteButton />
+        <UpvoteButton count={votes?.upvote} liked={liked}/>
+        <DownvoteButton disliked={disliked}/>
         <input name="post_id" class="hidden" value={post_id}>
     </form>
     <a href={`/post/${post_id}#comment`} class="flex flex-row  items-center gap-3 p-2 hover:bg-gray-100 rounded-md text-gray-400"><MessageSquare class="w-5 h-5"/> 55 <span class="max-md:hidden">comments</span></a>
